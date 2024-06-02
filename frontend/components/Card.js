38e230cce66e8b031ceabbbe2f9477afcb36694f
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-06-02 13:51:50 -0700

    wip

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Figure from "./Figure"
-import Styled from 
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+  border: 2px solid black;
+  padding: 1em;
+  border-radius: 2em;
+
+  h2 {
+    color: ${props => props.$color};
+    font-size: 2em;
+    margin: 0 0 1rem 0;
+  }
+ p {
+  margin: 0;
+
+  &::first-line {
+    font-size: 1.5em;
+  }
+ }
+`
 
 export default function Card({title, text, image, author, date}) {
     return (
-      <div className='card'>
-        <h1>{title}</h1>
+      <StyledCard $color="orange" className='card'>
+        <h2>{title}</h2>
         <p>{text}</p>
         <Figure image={image} author={author} dat={date}/>
-      </div>
+      </StyledCard>
     )}
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,9 +1,22 @@
 import React from "react"
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure`
+  max-width: 75%;
+  display: block;
+  align-items: center;
+
+  figcaption {
+    font-style:italic; 
+  }
+
+`
+
 
 export default function Figure({image, author, date}){
     return (
-      <figure>
+      <StyledFigure>
           <img src={image} alt="NASA APOD"/>
           <figcaption>The Photo of the Day by {author} on {date}</figcaption>
-      </figure>
+      </StyledFigure>
     )}
